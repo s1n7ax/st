0f651517c51a1c42677caaee5f29172af0f8dfd2
s1n7ax <srineshnisala@gmail.com>
2019-10-11 23:51:43 +0530

    ADD: st-alpha
    * patch st-alpha-0.8.2.diff
    * details: this patch allows transparent terminal windows
    * NOTE: you need xcompmgr running, in order this to effect

--- config.def.h
+++ config.def.h
@@ -88,6 +88,9 @@ char *termname = "st-256color";
  */
 unsigned int tabspaces = 8;
 
+/* bg opacity */
+float alpha = 0.8;
+
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 
@@ -121,7 +124,7 @@ static const char *colorname[] = {
  * foreground, background, cursor
  */
 unsigned int defaultfg = 257;
-unsigned int defaultbg = 256;
+unsigned int defaultbg = 258;
 static unsigned int defaultcs = 257;
 static unsigned int defaultrcs = 257;
 
